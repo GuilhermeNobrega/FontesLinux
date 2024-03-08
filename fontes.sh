95a118bf05f931f1fc003b9bc8871a272cf5bc2c
Wilhelm <100214869+GuilhermeNobrega@users.noreply.github.com>
2024-03-07 22:38:56 -0300

    Update fontes.sh

--- fontes.sh
+++ fontes.sh
@@ -18,27 +18,50 @@ fontes=("banner" "lean" "small" "big" "mini" "smscript" "block" "mnemonic" "smsh
 #echo "${fontes[@]}"
 #figlet -cf lean $frase
 function exec(){
+    while true
+    do
     c=0
     for i in "${fontes[@]}";
     do
         echo -e "\t\t---------------------$i---------------------"
     done
     echo -e "\n"
-    read -p "--------------Digite a frase que você deseja:-------------- " frase
-    read -p "--------------Digite a fonte que você deseja:-------------- " fonte
-    read -p "--------------Quer ver todas as fontes em uma frase:-------------- " resposta
-    if [[ " ${fontes[@]} " =~ " $fonte " ]];
+    echo "---------------------------"
+    echo "| Opções do Script         |"
+    echo "---------------------------"
+    echo -e "| 1. Digitar Frase         |"
+    echo -e "| 2. Ver Fonte Específica  |"
+    echo -e "| 3. Ver Todas as Fontes   |"
+    echo  "---------------------------"
+    echo -e "\n"
+    read -p "Digite a sua opção: " resposta
+
+    if [[ $resposta == 1 ]];
     then
-        echo "Existe essa fonte"
+        read -p "Digite a sua frase: " frase
+
+        figlet $frase
+        sleep 1
+    elif [[ $resposta == 2 ]];
+    then
+        read -p "Digite a fonte que você quer ver:" fonte
+        read -p "Digite a sua frase: " frase
         figlet -cf $fonte $frase
-    elif [[ $resposta == "sim" || $resposta == "Sim" ]];
+        sleep 1
+
+    elif [[ $resposta == 3 ]];
     then
-        for todos in "${fontes[@]}";
+        read -p "Digite a sua frase: " frase
+        for i in ${fontes[@]}
         do
-            figlet -cf $todos bandit
+            figlet -cf $i $frase
+            sleep 0.5
         done
+
     else
-        echo "Não existe essa fonte"
+        echo "...Values not compatible..."
+        sleep 1
     fi
+    done
 }   
 exec
